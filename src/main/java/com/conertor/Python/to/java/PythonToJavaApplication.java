422cba2c3af04870439b5d72206c94587661681a
ayushchaudhary21 <chaudharyayush269@gmail.com>
2025-07-10 19:07:47 +0530

    pthon to java Converrtor

---
+++ src/main/java/com/conertor/Python/to/java/PythonToJavaApplication.java
@@ -0,0 +1,13 @@
+package com.conertor.Python.to.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PythonToJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PythonToJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conertor/Python/to/java/PythonToJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conertor.Python.to.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PythonToJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
